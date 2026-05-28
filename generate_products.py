dab8d9bde9872adc6c48ffd9c4a155524f506c62
Alex Anahart <alex.anahart@example.com>
2026-05-28 01:02:37 +0100

    jr

--- generate_products.py
+++ generate_products.py
@@ -8,8 +8,8 @@ repo_root = os.path.dirname(os.path.abspath(__file__))
 os.chdir(repo_root)
 
 # === Параметры ===
-csv_path = "maroc_ladys_01.csv"
-html_path = "maroc_ladys_01.html"
+csv_path = "maroc.csv"
+html_path = "maroc.html"
 images_dir = "images"
 valid_exts = {".jpg", ".jpeg", ".png"}
 
